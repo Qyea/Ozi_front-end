9045726ba9d3b96ccfa59ebd86d067b411ff0eca
Qyea <kreshess@mail.ru>
2023-03-17 18:37:00 +0300

    modidicatin of page

--- js/main.js
+++ js/main.js
@@ -1,71 +1,34 @@
-(function () {
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-      if (window.pageYOffset > 150){
-          header.classList.add('header_active');
-      }else{ header.classList.remove('header_active');
-      }  
-    };
- }());
+const selectImage = document.querySelector('.select-image');
+const inputFile = document.querySelector('#file');
+const imgArea = document.querySelector('.img-area');
 
-//Burger handler
+selectImage.addEventListener('click', function () {
+	inputFile.click();
+})
 
-(function () {
-    const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('.header__nav');
-    const menuCloseItem = document.querySelector('.header__nav-close');
-    const menuLinks = document.querySelectorAll('.header__link');
-    burgerItem.addEventListener('click',()=>{
-        menu.classList.add('header__nav_active');
-    });
-    menuCloseItem.addEventListener('click',() =>{
-        menu.classList.remove('header__nav_active');
-    });
-    if(window.innerWidth <= 767){
-        for (let i = 0; i < menuLinks.length; i++){
-            menuLinks[i].addEventListener('click', () =>{
-                menu.classList.remove('header__nav_active');
-            });
-        }
-    }
-}());
+inputFile.addEventListener('change', function () {
+	const image = this.files[0]
+	if(image.size < 2000000) {
+		const reader = new FileReader();
+		reader.onload = ()=> {
+			const allImg = imgArea.querySelectorAll('img');
+			allImg.forEach(item=> item.remove());
+			const imgUrl = reader.result;
+			const img = document.createElement('img');
+			img.src = imgUrl;
+			imgArea.appendChild(img);
+			imgArea.classList.add('active');
+			imgArea.dataset.img = image.name;
+		}
+		reader.readAsDataURL(image);
+	} else {
+		alert("Image size more than 2MB");
+	}
+})
 
-// Scroll to anchors
-
-(function () {
-    
-    const smoothScroll = function (targetEl, durection){
-        const headerElHeight = document.querySelector('.header').clientHeight;
-        let target = document.querySelector(targetEl);
-        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-        let startPosition = window.pageYOffset;
-        let startTime = null;
-    
-        const ease = function(t,b,c,d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t *(t - 2) - 1) + b;
-    };
-        
-        const animation = function(currentTime){
-            if (startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime - startTime;
-            const run = ease(timeElapsed, startPosition, targetPosition, durection);
-            window.scrollTo(0,run);
-            if(timeElapsed < durection) requestAnimationFrame(animation);
-        };
-        requestAnimationFrame(animation);
-    };
-    
-    const scrollTo = function (){
-        const links = document.querySelectorAll('.js-scroll');
-        links.forEach(each => {
-            each.addEventListener('click', function() {
-                const currentTarget = this.getAttribute('href');
-                smoothScroll(currentTarget,1000);                
-            });
-        });    
-    };
-    scrollTo();
- }());
+document.querySelector(".get__registratoin").addEventListener('click', ()=>{
+    //let data = document.getElementsByName("name").value;
+    //console.log(data);
+    //document.querySelector('.form__label').innerHTML = data;
+    document.querySelector('input[name="name"]').value = 123;
+})
